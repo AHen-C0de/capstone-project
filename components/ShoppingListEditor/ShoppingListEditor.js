7b840f023571f0e0b251f88eb27624c21f7085c4
Angela Hentschel <angela.neuron@gmail.com>
2022-10-19 16:58:45 +0200

    add basic layout of list item in editor and add delete button

--- components/ShoppingListEditor/ShoppingListEditor.js
+++ components/ShoppingListEditor/ShoppingListEditor.js
@@ -5,13 +5,30 @@ import ListContainer from "../ListContainer";
 export default function ShoppingListEditor({ items }) {
   return (
     <ListContainer>
-      {items.map(({ id, name }) => (
-        <li key={id}>{name}</li>
-      ))}
+      <List>
+        {items.map(({ id, name }) => (
+          <ListItemContent key={id}>
+            <p>{name}</p>
+            <DeleteButton>Löschen</DeleteButton>
+          </ListItemContent>
+        ))}
+      </List>
     </ListContainer>
   );
 }
 
-const StyledList = styled.ul`
+const List = styled.ul`
   list-style: none;
+  line-height: 2rem;
+`;
+
+const ListItemContent = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const DeleteButton = styled.button`
+  background-color: red;
+  height: 1.5rem;
 `;
