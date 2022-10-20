9ac72dce671d09867236b690ba05ab66d6ded733
Angela Hentschel <angela.neuron@gmail.com>
2022-10-20 16:21:09 +0200

    re-factor: lift shoppingList state up into App component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 import GlobalStyle from "../components/GlobalStyle";
+import { shopping_list_DB } from "../services/db.js";
 
 function MyApp({ Component, pageProps }) {
+  const [shoppingListItems, setShoppingListItems] = useState(shopping_list_DB);
+
+  function toggleItemChecked(id) {
+    const updatedItems = shoppingListItems.filter((item) => item.id !== id);
+    const toggledItem = shoppingListItems.find((item) => item.id === id);
+    toggledItem.checked = !toggledItem.checked;
+    updatedItems.unshift(toggledItem);
+
+    setShoppingListItems(updatedItems);
+  }
+
+  function deleteItem(id) {
+    setShoppingListItems((previousItems) =>
+      previousItems.filter((item) => item.id !== id)
+    );
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        items={shoppingListItems}
+        onToggleItemChecked={toggleItemChecked}
+        onDelete={deleteItem}
+      />
     </>
   );
 }
