267a224cae5eccbd84f7613062981a1167b29474
Angela Hentschel <angela.neuron@gmail.com>
2022-10-24 17:18:32 +0200

    add basic styling to InputDropDown component

--- components/ShoppingListEditor/InputDropDown.js
+++ components/ShoppingListEditor/InputDropDown.js
@@ -1,9 +1,24 @@
+import styled from "styled-components";
+
 export default function InputDropDown({ optionElements }) {
   return (
-    <ul>
+    <List>
       {optionElements.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+        <li key={id}>
+          <StyledButton>{name}</StyledButton>
+        </li>
       ))}
-    </ul>
+    </List>
   );
 }
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  line-height: 1rem;
+  list-style: none;
+`;
+
+const StyledButton = styled.button`
+  width: 100%;
+`;
