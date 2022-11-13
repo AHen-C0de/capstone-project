4bd26fa25d2667534378eb39fab4fc59c3d2bd49
Angela Hentschel <angela.neuron@gmail.com>
2022-11-13 11:59:57 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import GlobalStyle from "/components/GlobalStyle";
-import Div100vh from "react-div-100vh";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Div100vh>
+    <>
       <GlobalStyle />
       <Component {...pageProps} />
-    </Div100vh>
+    </>
   );
 }
 
