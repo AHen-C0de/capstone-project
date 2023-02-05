9fffb592fbfb84a2ed3a5bd4c1eb70eacb30061a
unknown <angela.neuron@gmail.com>
2023-02-05 20:13:13 +0100

    add Header

--- pages/add.js
+++ pages/add.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import Header from "../components/Header";
+import SignIn from "../components/SignIn";
+import SignOutButton from "../components/buttons/SignOutButton";
 
 export default function Add() {
     return (
@@ -8,6 +11,9 @@ export default function Add() {
         <Head>
           <title>Produkte hinzufügen</title>
         </Head>
+        <Header text="Produkte hinzufügen">
+          {/* <SignOutButton onSignOut={signOut} /> */}
+        </Header>
         <main>
 
         </main>
